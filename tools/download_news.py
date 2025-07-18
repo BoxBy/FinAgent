7b98e98793ec9c76c903e94921ba9a118cd2e65f
minji <sasmco12@gmail.com>
2025-07-18 13:32:16 +0900

    api process limit

--- tools/download_news.py
+++ tools/download_news.py
@@ -10,6 +10,8 @@ import argparse
 from mmengine.config import Config, DictAction
 import multiprocessing
 
+import time
+
 ROOT = str(Path(__file__).resolve().parents[1])
 sys.path.append(ROOT)
 
@@ -26,7 +28,8 @@ class StockDownloaderProcess(multiprocessing.Process):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download News')
-    parser.add_argument("--config", default=os.path.join(ROOT, "configs", "downloader", "news", "fmp_news_exp_cryptos.py"), help="download datasets config file path")
+    # parser.add_argument("--config", default=os.path.join(ROOT, "configs", "downloader", "news", "fmp_news_exp_cryptos.py"), help="download datasets config file path")
+    parser.add_argument("--config", default=os.path.join(ROOT, "configs", "downloader", "news", "yahoofinance_news_dj30.py"), help="download datasets config file path")
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -80,17 +83,26 @@ def main():
 
     batch_size = cfg.batch_size if cfg.batch_size < len(stocks) else 5
     batch_size = min(len(stocks), batch_size)
+    max_processes = 2  # 동시 실행 프로세스 수 제한
 
     processes = []
     remaining_stocks = downloader.check_download()
 
     while remaining_stocks:
-        batch = remaining_stocks[:batch_size]
-        remaining_stocks = remaining_stocks[batch_size:]
+        # 현재 실행 중인 프로세스 수 확인
+        active_processes = [p for p in processes if p.is_alive()]
+        if len(active_processes) < max_processes:
+            batch = remaining_stocks[:batch_size]
+            remaining_stocks = remaining_stocks[batch_size:]
 
-        process = StockDownloaderProcess(batch, downloader)
-        processes.append(process)
-        process.start()
+            process = StockDownloaderProcess(batch, downloader)
+            processes.append(process)
+            process.start()
+
+        # 프로세스 시작 간격 추가
+            time.sleep(2.0)
+        else:
+            time.sleep(1.0)  # 잠시 대기
 
     for process in processes:
         process.join()
